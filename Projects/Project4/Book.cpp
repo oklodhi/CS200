7ae6ae195df22b133bfdfbe4e01644c3b87e3025
oklodhi <okhanlodhi@gmail.com>
2018-07-20 10:18:21 -0500

    Initial Commit

---
+++ Projects/Project4/Book.cpp
@@ -0,0 +1,40 @@
+#include "Book.hpp"
+void Book::SetBookInfo( const string& title, const string& author )
+{
+}
+
+void Book::SetReadingStatus( ReadingStatus rs )
+{
+}
+
+void Book::SetPurchaseStatus( PurchaseStatus ps )
+{
+}
+
+void Book::DisplayBook()
+{
+}
+
+string Book::GetTitle()
+{
+}
+
+string Book::GetAuthor()
+{
+}
+
+int Book::GetReadingStatus()
+{
+}
+
+string Book::GetReadingStatusString()
+{
+}
+
+int Book::GetPurchaseStatus()
+{
+}
+
+string Book::GetPurchaseStatusString()
+{
+}
---
+++ Projects/Project4/Book.hpp
@@ -0,0 +1,32 @@
+#ifndef BOOK_HPP_INCLUDED
+#define BOOK_HPP_INCLUDED
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum ReadingStatus { NOT_STARTED = 0, READING = 1, FINISHED = 2 };
+enum PurchaseStatus { OWNED = 0, WISHLIST = 1 };
+
+class Book {
+    private:
+        string m_title;
+        string m_author;
+        ReadingStatus m_readingStatus;
+        PurchaseStatus m_puchaseStatus;
+
+
+    public:
+        void DisplayBook();
+        int GetPurchaseStatus();
+        string GetPurchaseStatusString();
+        int GetReadingStatus();
+        string GetReadingStatusString();
+        string GetTitle();
+        void SetBookInfo (const string& title, const string& author);
+        void SetPurchaseStatus (PurchaseStatus ps);
+        void SetReadingStatus (ReadingStatus rs);
+};
+
+#endif // BOOK_HPP_INCLUDED
