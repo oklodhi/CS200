0967a88b9ab2f2da4e2f441c5a74745b79196aa9
oklodhi <okhanlodhi@gmail.com>
2018-07-22 12:56:23 -0500

    Completed Lab15

--- Labs/Lab15/Question.cpp
+++ Labs/Lab15/Question.cpp
@@ -1,6 +1,61 @@
+#include <iostream>
 #include "Question.h"
+using namespace std;
 
-QuestionBase::display(){
-    cout << m_questionText;
+void QuestionBase::Display(){
+    cout << m_questionText << endl;
+}
 
+
+void QuestionBase::SetQuestionText(const string& text){
+    m_questionText = text;
+}
+
+void TrueFalseQuestion::Display(){
+    QuestionBase::Display();
+    cout << "*true* OR *false*" << endl;
+}
+
+void TrueFalseQuestion::SetCorrectAnswer(const string& correctAnswer){
+    m_questionText = correctAnswer;
+}
+
+bool TrueFalseQuestion::CheckAnswer(const string& userAnswer){
+    if (m_correctAnswer == userAnswer) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void MultipleChoiceQuestion::Display(){
+    QuestionBase::Display();
+    for (int i = 0; i < 4; i++) {
+        cout << i << ". " << m_answers[i] << endl;
+    }
+}
+
+void MultipleChoiceQuestion::SetCorrectAnswer(int correctAnswer){
+    m_questionText = correctAnswer;
+}
+
+bool MultipleChoiceQuestion::CheckAnswer(int userAnswer){
+    if (userAnswer == 0){
+        return true;
+    } else if (userAnswer == 1){
+        return true;
+    } else if (userAnswer == 2){
+        return true;
+    } else if (userAnswer == 3){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void MultipleChoiceQuestion::SetAnswerChoices(const string& a0, const string& a1, const string& a2, const string& a3){
+    m_answers[0] = a0;
+    m_answers[1] = a1;
+    m_answers[2] = a2;
+    m_answers[3] = a3;
 }
--- Labs/Lab15/Question.h
+++ Labs/Lab15/Question.h
@@ -1,12 +1,38 @@
 #ifndef QUESTION_H_INCLUDED
 #define QUESTION_H_INCLUDED
+#include <string>
+using namespace std;
 
-class QuestionBase() {
-    protected string m_questionText;
+class QuestionBase {
+    protected:
+        string m_questionText;
 
     public:
-        void display();
-        void setQuestionText(const string& text);
+        void Display();
+        void SetQuestionText(const string& text);
+};
+
+class TrueFalseQuestion : public QuestionBase {
+    private:
+        string m_correctAnswer;
+
+    public:
+        void Display();
+        bool CheckAnswer(const string& userAnswer);
+        void SetCorrectAnswer(const string& correctAnswer);
+};
+
+class MultipleChoiceQuestion : public QuestionBase {
+    private:
+        int m_correctAnswer;
+        string m_answers[4];
+
+    public:
+        void Display();
+        bool CheckAnswer(int userAnswer);
+        void SetCorrectAnswer(int correctAnswer);
+        void SetAnswerChoices(const string& a0, const string& a1, const string& a2, const string& a3);
+
 };
 
 #endif // QUESTION_H_INCLUDED
