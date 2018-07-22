abc9d932bbfb801ee44dcbb830cc75b7cb2e5d6d
oklodhi <okhanlodhi@gmail.com>
2018-07-22 11:43:01 -0500

    Remove CodeBlocks project files

--- Projects/Project3/CourseList.cpp
+++ Projects/Project3/CourseList.cpp
@@ -7,34 +7,34 @@ void CourseList::Setup()
 {
 	m_courseCount = 0;
 	m_arraySize = 5;
-	m_array = new string[5]; 
+	m_array = new string[5];
 }
 
 void CourseList::Cleanup()
 {
-	delete[] m_array; 
+	delete[] m_array;
 }
 
 void CourseList::Add( const string& item )
 {
 	if (IsFull()) {
-		Resize(); 
+		Resize();
 	}
 
-	int availIndex = FindAvailableIndex(); 
+	int availIndex = FindAvailableIndex();
 
-	m_array[availIndex] = item; 
+	m_array[availIndex] = item;
 
-	m_courseCount++; 
+	m_courseCount++;
 }
 
 bool CourseList::IsFull()
 {
 	if (m_courseCount == m_arraySize) {
-		return true; 
+		return true;
 	}
 	else {
-		return false; 
+		return false;
 	}
 }
 
@@ -42,26 +42,26 @@ int CourseList::FindAvailableIndex()
 {
 	for (int i = 0; i < m_arraySize; i++) {
 		if (m_array[i] == "") {
-			return i; 
+			return i;
 		}
 	}
-	return -1; 
+	return -1;
 }
 
 void CourseList::Remove( int index )
 {
 	if (!IsValidIndex(index)) {
 		cout << "Error: Invalid index" << endl;
-		return; 
+		return;
 	}
 	else if (m_array[index] == ""){
 		cout << "Error: Nothing in index" << endl;
 		return;
 	}
 	else {
-		m_array[index] = ""; 
-		m_courseCount--; 
-		cout << "Item at index " << index << " removed." << endl; 
+		m_array[index] = "";
+		m_courseCount--;
+		cout << "Item at index " << index << " removed." << endl;
 	}
 }
 
@@ -71,14 +71,14 @@ string CourseList::GetCourse( int index )
 		return m_array[index];
 	}
 	else {
-		return ""; 
+		return "";
 	}
 }
 
 void CourseList::Display()
 {
-	for (int i = 0; i < m_array->size; i++) {
-		cout << "Index " << i << " :" << m_array[i] << endl; 
+	for (int i = 0; i < m_arraySize; i++) {
+		cout << "Index " << i << " :" << m_array[i] << endl;
 	}
 }
 
@@ -88,32 +88,32 @@ bool CourseList::IsValidIndex( int index )
 		return true;
 	}
 	else {
-		return false; 
+		return false;
 	}
 }
 
 int CourseList::Size()
 {
-	return m_arraySize; 
+	return m_arraySize;
 }
 
 int CourseList::CourseCount()
 {
-	return m_courseCount; 
+	return m_courseCount;
 }
 
 void CourseList::Resize()
 {
-	int newSize = m_arraySize + 5; 
+	int newSize = m_arraySize + 5;
 
-	string * newArray = new string[newSize]; 
+	string * newArray = new string[newSize];
 
 	for (int i = 0; i < m_arraySize; i++) {
-		newArray[i] = m_array[i]; 
+		newArray[i] = m_array[i];
 	}
 
-	delete[] m_array; 
+	delete[] m_array;
 
-	m_array = newArray; 
-	m_arraySize = newSize; 
+	m_array = newArray;
+	m_arraySize = newSize;
 }
